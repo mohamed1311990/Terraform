fc6cfd78c07b5cd158feaaaef754bf2f2e766af8
mohamed1311990 <mohamed.salem.say@gmail.com>
2023-01-19 12:58:45 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,4 @@ resource "aws_instance" "testing" {
   ami           = "ami-0fe0b2cf0e1f25c8a" # eu-west-1
   instance_type = "t2.micro"
 
-  network_interface {
-    network_interface_id = aws_network_interface.testing.id
-    device_index         = 0
-  }
 }
